a91607891f85225730d41cdc6325a3ee501e488e
Manuel de Jesús González Arvelo <manuelarvelo19@gmail.com>
2018-06-29 09:22:12 +0200

    setting up git

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Header from './src/components/header';
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Header headerText={'Albums!'} />      
+
+    );
+  }
+}
