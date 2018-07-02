9450021685fb1869902b903a48c79d13f0687753
Manuel Gonzalez <manuelarvelo@hotmail.com>
2018-07-02 08:43:37 +0200

    albums list y details

---
+++ src/components/AlbumList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    state = {albums: []};
+
+    componentWillMount(){
+        fetch('https://rallycoding.herokuapp.com/api/music_albums').then((response)=>{            
+            return response.json();
+        }).then((data)=>{
+            this.setState({albums: data});
+        })
+    }
+    renderAlbums(){
+        return this.state.albums.map((album)=> <AlbumDetail key={album.title} album={album} />);
+    }
+  
+    render() {
+      return (
+        <View>
+          {this.renderAlbums()}
+        </View>
+      );
+    }
+  }
+
+
+export default AlbumList;
