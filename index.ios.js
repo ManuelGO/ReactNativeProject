64fb406d51a0b3166c6f807043bbfe356c1912d3
Manuel de Jesús González Arvelo <manuelarvelo19@gmail.com>
2018-07-02 20:36:21 +0200

    borrado archivo index.ios.js

--- index.ios.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text, AppRegistry } from 'react-native';
-
-const App = () => (
-        <Text>Some Text</Text>
-    );
-
-AppRegistry.registerComponent('albums', () => App);
